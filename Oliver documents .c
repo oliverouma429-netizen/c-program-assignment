a2ab7129394b5da6946351f9b678db8f5455a4f9
Oliver <oliverouma429@gmail.com>
2025-09-24 00:51:23 +0300

    Add files via upload

---
+++ Oliver documents .c
@@ -0,0 +1,27 @@
+//
+/*
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+int bankbalance ;
+int phonenumber ;
+int height ;
+
+printf("enter your bankbalance: \n");
+scanf("%d",& bankbalance);
+printf("your bankbalance is %d.\n", bankbalance);
+
+printf("enter your phonenumber: \n");
+scanf("%d",& phonenumber);
+printf("your phonenumber is %d.\n", phonenumber);
+
+printf("enter your height:\n");
+scanf("%d",& height);
+printf("your height is %d.\n", height);
+
+    return 0;
+}
