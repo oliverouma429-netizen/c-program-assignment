a7414318aa179d365127c5fdf4a15ae8aba38bd1
Oliver <oliverouma429@gmail.com>
2025-10-02 18:51:34 +0500

    Add files via upload

---
+++ Water bill program.c
@@ -0,0 +1,28 @@
+//program to output water bill
+/*
+Name 
+Reg No 
+Description 
+*/
+
+#include<stdio.h>
+int main(){
+int water_units;
+float bills;
+
+    printf("Enter number of water units:");
+    scanf("%d",&water_units);
+    
+    if(water_units>0&&water_units<=30)
+    {
+    bills=20*water_units;
+    printf("Your total bill is %.2f",bills);
+    }
+    else if (water_units>60)
+    {
+    bills=30*water_units;
+    printf("Your total bill is%.2f",bills);
+    }
+    
+    return 0;
+}
